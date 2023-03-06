0affc067a4d2ea1f50c5d0167302f446307d7e10
Your Name <you@example.com>
2023-03-06 22:08:12 +0000

    adjustin jsc

--- src/components/react/ReactHero.jsx
+++ src/components/react/ReactHero.jsx
@@ -45,21 +45,7 @@ export default function Home() {
 
             </div>
 
-            <style jsx>{`
-      `}</style>
 
-            <style jsx global>{`
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
         </div>
     )
 }
